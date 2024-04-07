63b2e538eedaa27be03135f9c70e670a3a185a51
jonichi <jonichi16@gmail.com>
2024-04-07 10:05:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODOS TODOS
+
+In development
---
+++ app/src/main/java/com/jonichidev/todo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.jonichidev.todo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.jonichidev.todo.ui.theme.TodoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                }
+            }
+        }
+    }
+}
